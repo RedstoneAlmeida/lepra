6b93f4602fceecd362e0c479040a9bc126464f0b
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-04-14 15:01:11 -0300

    Added files via upload

---
+++ softmine/level/generator/GeneratorRegisterTask.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+namespace softmine\level\generator;
+
+use softmine\block\Block;
+
+use softmine\level\generator\biome\Biome;
+use softmine\level\Level;
+use softmine\level\SimpleChunkManager;
+use softmine\scheduler\AsyncTask;
+
+use softmine\utils\Random;
+
+class GeneratorRegisterTask extends AsyncTask{
+
+	public $generator;
+	public $settings;
+	public $seed;
+	public $levelId;
+
+	public function __construct(Level $level, Generator $generator){
+		$this->generator = get_class($generator);
+		$this->settings = $generator->getSettings();
+		$this->seed = $level->getSeed();
+		$this->levelId = $level->getId();
+	}
+
+	public function onRun(){
+		Block::init();
+		Biome::init();
+		$manager = new SimpleChunkManager($this->seed);
+		$this->saveToThreadStore("generation.level{$this->levelId}.manager", $manager);
+		/** @var Generator $generator */
+		$generator = $this->generator;
+		$generator = new $generator($this->settings);
+		$generator->init($manager, new Random($manager->getSeed()));
+		$this->saveToThreadStore("generation.level{$this->levelId}.generator", $generator);
+	}
+}
